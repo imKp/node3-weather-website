c5aadcd3d3dc9ac7313473d2229191dc936a58c7
Kunal Pathak <kunalpath@gmail.com>
2020-05-23 10:17:39 +0530

    Removed unneceasey comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) =>{
-//         if(data.erro){
-//             console.log(data.error)
-//         }else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }      
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
